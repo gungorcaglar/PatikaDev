74fcaff9cd13311c0b85809a1b75533dc61c9865
Caglar <gungorcaglar@hotmail.com.tr>
2023-01-14 17:18:26 +0300

    update

---
+++ SolidityPractium/Counter/Counter.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.8.7;
+
+contract Counter {
+    uint public count;
+
+    function inc() external {
+        count += 1;
+    }
+
+    function dec() external {
+        count -= 1;
+    }
+}
